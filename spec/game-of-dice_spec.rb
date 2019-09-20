6f3e40296a30df73106555c197eb5928059f5605
Heli Sivunen <heli@mooncat.tv>
2019-09-20 11:45:38 +0100

    User can roll an exact number of dice.

--- spec/game-of-dice_spec.rb
+++ spec/game-of-dice_spec.rb
@@ -8,10 +8,19 @@ describe GameOfDice do
     expect(actual).to eq(expected)
   end
 
-  it 'initialises dice with 12 sides' do
-    dice = GameOfDice.new(12)
-    actual = dice.sides
-    expected = 12
+  it 'can roll a 6 dice' do
+    dice = GameOfDice.new
+    roll = dice.roll(6)
+    actual = roll.size
+    expected = 6
+    expect(actual).to eq(expected)
+  end
+
+  it 'generates random numbers from 1 to 6' do
+    dice = GameOfDice.new
+    roll = dice.roll(100)
+    actual = roll.minmax
+    expected = [1,6]
     expect(actual).to eq(expected)
   end
 end
